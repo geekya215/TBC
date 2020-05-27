8e8cf1adb561173c2be21b5c1620082c95527dc5
Lin Yang <geekya215@gmail.com>
2020-05-27 10:15:36 +0800

    feat: bind web3 to build-in object app

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Web3 = require('web3');
+
 module.exports = app => {
   app.config.coreMiddleware.unshift('jwtErrorHandler');
+  app.web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
 };
